16f5013db25ee72c03756542eae04e7c42b36079
dash4274 <hayalu4hb@gmail.com>
2024-10-26 18:55:39 +0300

    commit main

--- src/Pages/Blog.tsx
+++ src/Pages/Blog.tsx
@@ -2,7 +2,7 @@ function Blog() {
   return (
     <div>
       Blog post of planet tutors which is about the different educational
-      advices we offer for us!
+      advices we offer for us! ' Zerihun Solomon'
     </div>
   );
 }
